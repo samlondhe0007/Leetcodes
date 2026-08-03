6060bb38bfcb6a83e78e193ae3904290d9e735c1
samlondhe0007 <samlondhe0007@gmail.com>
2026-08-03 15:44:07 +0530

    Time: 23 ms (5.59%), Space: 42.5 MB (79.62%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,17 @@
+    class Solution {
+        public int mySqrt(int x) {
+            
+            int ans =0;
+            for (int i=1;i<=x;i++){
+                if ((long)i*i<=x){
+                    ans=i;
+
+                }
+                else{
+                    break;
+                }
+
+            }
+            return ans;
+        }
+    }

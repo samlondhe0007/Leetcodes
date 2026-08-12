712d743a8c0a6bbd692ed53b9f702da2a979bbba
Samarth <samlondhe0007@gmail.com>
2026-08-12 20:37:38 +0530

    Time: 15 ms (14.46%), Space: 47.1 MB (27.34%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,18 +1,16 @@
 class Solution {
     public String reverseWords(String s) {
-          
-     String[]arr=s.split(" ");
-     String result="";
-        for (int i=0;i<arr.length;i++){
-            String reverse = "";
-           for (int j = arr[i].length() - 1; j >= 0; j--) {
-           reverse = reverse + arr[i].charAt(j);
 
-            }
-            result = result+reverse+" ";
+        String[] arr = s.split(" ");
+        String result = "";
 
+        for (int i = 0; i < arr.length; i++) {
+
+            StringBuilder sb = new StringBuilder(arr[i]);
+
+            result = result + sb.reverse() + " ";
         }
-        return result.trim(); 
-        
+
+        return result.trim();
     }
 }

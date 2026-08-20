33f730b0efcd9673947d71a5678390cc817a81d8
Samarth Londhe <samlondhe0007@gmail.com>
2026-08-21 00:25:41 +0530

    Time: 2 ms (84.29%), Space: 47.5 MB (53.99%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if (arr.length<3){
+            return false;
+        }
+
+        int peak =Integer.MIN_VALUE;
+        int index=0;
+
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]>peak){
+                peak = arr[i];
+                index=i;
+
+            }
+
+        }
+
+        if (index==0||index==arr.length-1){
+            return false;
+
+        }
+
+        for (int i=0;i<index;i++){
+            if (arr[i]>=arr[i+1]){
+                return false;
+            }
+
+        }
+
+        for (int i=index;i<arr.length-1;i++){
+            if (arr[i]<=arr[i+1]){
+                return false;
+
+            }
+
+        }
+
+       
+       return true;
+        
+    }
+}

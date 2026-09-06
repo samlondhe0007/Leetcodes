69d9e837dc5a67a810a8e1654951b3b26561c0e4
Samarth Londhe <samlondhe0007@gmail.com>
2026-09-06 23:51:20 +0530

    Time: 1 ms (100%), Space: 43.7 MB (11.84%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxPower(String s) {
+        int max=1;
+        int count=1;
+        for (int i=1;i<s.length();i++){
+            if (s.charAt(i)==s.charAt(i-1)){
+                count++;
+            }
+            else{
+            max=Math.max(max,count);
+            count=1;
+            }
+
+        }
+        max=Math.max(max,count);
+        return max;
+    }
+}

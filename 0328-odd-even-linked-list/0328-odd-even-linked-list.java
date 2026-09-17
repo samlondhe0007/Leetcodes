4030985413b0deff5048bb3c3a8305a8f30165ef
Samarth Londhe <samlondhe0007@gmail.com>
2026-09-17 23:54:05 +0530

    Time: 0 ms (100%), Space: 46.5 MB (35.87%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -14,20 +14,17 @@ class Solution {
           if (head == null || head.next == null) {
             return head;
         }
-
-
         ListNode odd = head;
         ListNode even = head.next;
         ListNode evenHead=even;
 
         while(even!=null && even.next!=null){
-            odd.next=even.next;
-            odd=odd.next;
-            even.next = odd.next;
-            even = even.next;
-            
-        }
+            odd.next=odd.next.next;
+            even.next = even.next.next;
 
+            odd=odd.next;
+            even = even.next;
+        }
         odd.next=evenHead;
 
             return head;
